9893e315ab41f9177b04fd9f8b9ffdc8ec6e6d15
dankiritu <daniel.kiritu@student.moringaschool.com>
2024-10-14 19:03:02 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,22 @@
 def write_file(file_name, file_content):
-    pass
+    """Writes content to a file."""
+    # Make sure to write to a file with the correct extension (.txt)
+    with open(f'{file_name}.txt', 'w') as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    """Appends content to a file."""
+    # Make sure to append to a file with the correct extension (.txt)
+    with open(f'{file_name}.txt', 'a') as file:
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    """Reads content from a file and returns it."""
+    # Make sure to read from a file with the correct extension (.txt)
+    try:
+        with open(f'{file_name}.txt', 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        return None
